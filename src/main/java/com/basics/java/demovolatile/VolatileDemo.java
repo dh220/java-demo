cfebece591fae5fcc94d9868a385bbbfde76bc7c
daihang <daihang@hellogeek.com>
2021-01-18 11:48:58 +0800

    init

---
+++ src/main/java/com/basics/java/demovolatile/VolatileDemo.java
@@ -0,0 +1,13 @@
+package com.basics.java.demovolatile;
+
+/**
+ * @Created with IDEA
+ * @author:DaiHang
+ * @Date:2021/1/18
+ * @Time:11:36
+ */
+public class VolatileDemo {
+    public static void main(String[] args) {
+
+    }
+}
